3d8ac87a28e1713ca75727b2a202321b58ecd166
Alfred Tso tp <alfredtso@gmail.com>
2023-04-16 15:16:41 +0800

    tmux plugin for neovim and fzf-man-widget

--- nvim/lua/alfred/packer.lua
+++ nvim/lua/alfred/packer.lua
@@ -5,12 +5,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
   use({ 
 	  'rose-pine/neovim', as = 'rose-pine',
 	  config = function()
@@ -19,12 +13,26 @@ return require('packer').startup(function(use)
 
   })
 
+  -- Treesitter
   use ({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
   use ('nvim-treesitter/playground')
+
+  -- Navigation
   use ('theprimeagen/harpoon')
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	-- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use ('christoomey/vim-tmux-navigator')
+
+  -- Editing
   use ('mbbill/undotree')
+
+  -- Git
   use ('tpope/vim-fugitive')
 
+  -- LSP
   use {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v2.x',
